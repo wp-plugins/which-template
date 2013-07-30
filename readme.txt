97614b296f386bb1d68f73a6881c5707a407063d
MMDeveloper <MMDeveloper@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-07-30 05:37:55 +0000

    This version allows you to see if a page is using the page.php, single.php, 404.php, archive.php, tag.php, category.php or search.php template.
    git-svn-id: https://plugins.svn.wordpress.org/which-template/tags/2.0@748159 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ readme.txt
@@ -0,0 +1,48 @@
+=== Which Template ===
+Contributors: MMDeveloper
+Donate link: 
+Tags: plugin, template, page, which, admin
+Requires at least: 3.3
+Tested up to: 3.5.2
+Stable tag: 2.0
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+Work out which template your page is currently looking at.
+
+== Description ==
+
+A wordpress plugin that helps you work out which template your page is currently using. If you login to wordpress, visit your site, in the admin bar you will see a link to the template your page is using. It should say something like "Template: ...". Click on it and you will navigate to the actual template in a new tab.
+
+== Installation ==
+
+1) Install WordPress 3.5 or higher
+
+2) Download the latest from:
+
+http://wordpress.org/extend/plugins/which-template
+
+3) Login to WordPress admin, click on Plugins / Add New / Upload, then upload the zip file you just downloaded.
+
+4) Activate the plugin.
+
+
+== Changelog ==
+
+= 2.0 =
+
+* This version allows you to see if a page is using the page.php, single.php, 404.php, archive.php, tag.php, category.php or search.php template.
+
+= 1.0 =
+
+* Initial Commit
+
+== Upgrade notice ==
+
+= 2.0 =
+
+* This version allows you to see if a page is using the page.php, single.php, 404.php, archive.php, tag.php, category.php or search.php template.
+
+= 1.0 =
+
+* Initial Commit
---
+++ which-template.php
@@ -0,0 +1,67 @@
+<?php
+/*
+Plugin Name: Which Template
+Plugin URI: http://wordpress.org/extend/plugins/which-template/
+Description: Helps the admin user work out which template a particular page is using.
+
+Installation:
+
+1) Install WordPress 3.5.2 or higher
+
+2) Download the latest from:
+
+http://wordpress.org/extend/plugins/which-template 
+
+3) Login to WordPress admin, click on Plugins / Add New / Upload, then upload the zip file you just downloaded.
+
+4) Activate the plugin.
+
+Version: 2.0
+Author: TheOnlineHero - Tom Skroza
+License: GPL2
+*/
+
+add_action("admin_bar_menu", "which_template_customize_menu");
+
+function which_template_customize_menu() {
+  global $wp_admin_bar;
+	$template = get_post_meta( get_the_id(), '_wp_page_template', true );
+	$template_link = "";
+	if ($template != "") {
+		$template_link = get_option("siteurl")."/wp-admin/theme-editor.php?file=".$template."&theme=".wp_get_theme()->Template;
+	} else if (is_page(get_the_id())) {
+		$template_link = get_option("siteurl")."/wp-admin/theme-editor.php?file=page.php&theme=".wp_get_theme()->Template;
+		$template = "page.php";
+	} else if (is_single(get_the_id())) {
+		$template_link = get_option("siteurl")."/wp-admin/theme-editor.php?file=single.php&theme=".wp_get_theme()->Template;
+		$template = "single.php";
+	}	else if (is_search(get_the_id())) {
+		$template_link = get_option("siteurl")."/wp-admin/theme-editor.php?file=search.php&theme=".wp_get_theme()->Template;
+		$template = "search.php";
+	} else if (is_category(get_the_id())) {
+		$template_link = get_option("siteurl")."/wp-admin/theme-editor.php?file=category.php&theme=".wp_get_theme()->Template;
+		$template = "category.php";
+	} else if (is_archive(get_the_id())) {
+		$template_link = get_option("siteurl")."/wp-admin/theme-editor.php?file=archive.php&theme=".wp_get_theme()->Template;
+		$template = "archive.php";
+	} else if (is_tag(get_the_id())) {
+		$template_link = get_option("siteurl")."/wp-admin/theme-editor.php?file=tag.php&theme=".wp_get_theme()->Template;
+		$template = "tag.php";
+	} else if (is_404(get_the_id())) {
+		$template_link = get_option("siteurl")."/wp-admin/theme-editor.php?file=404.php&theme=".wp_get_theme()->Template;
+		$template = "404.php";
+	}
+
+	if ($template_link != "") {
+		$wp_admin_bar->add_menu(
+			array(
+			  "id" => "whichtemplatemenu",
+			  "title" => "Template: ".$template,
+			  "href" => $template_link,
+			  "meta" => array("target" => "blank")
+			)
+		);
+	}
+}
+
+?>
